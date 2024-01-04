b0104a4bbe22d30d57e86cb71b9bb99ac0a683f2
arshedgithub <arshedahmedmail@gmail.com>
2024-01-05 01:06:36 +0530

    add routes

---
+++ application/controllers/PageController.php
@@ -0,0 +1,16 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class PageController extends CI_Controller {
+
+	
+	public function index()
+	{
+        echo "This is Page Controller Page";
+	}
+
+	public function demo()
+	{
+        echo "demo sub route of Page Controller function";
+	}
+}
